2794d16d3a42c5f48ef0d39f77a59d2859ed7efe
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-23 19:51:06 -0300

    ...

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pessoas.urls')),
     path('admin/', admin.site.urls),
+    path('criar', include('pessoas.urls')),
+    path('editar', include('pessoas.urls')),
 ]
--- pessoas/views.py
+++ pessoas/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def criar(request):
+    return render(request, 'index2.html')
+
+def editar(request):
+    return render(request, 'index3.html')
