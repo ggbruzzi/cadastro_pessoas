f4c418b0ca24a170bcbe744e7b63595c9cdf65bc
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-23 13:11:55 -0300

    ...

--- cadastro/settings.py
+++ cadastro/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cadastro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cadastro_pessoas',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5433' 
     }
 }
 
--- pessoas/models.py
+++ pessoas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome_pessoa = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    idade = models.PositiveIntegerField()
+    data_nascimento = models.DateField(default=datetime.now, blank=True)
+    email = models.EmailField()
+    apelido = models.CharField(max_length=100)
+    observação = models.CharField(max_length=100)
