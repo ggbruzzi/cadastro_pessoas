60f6d034bed7ce5f4210844713294248c33b0b79
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-23 19:21:52 -0300

    finalizando !!

--- pessoas/views.py
+++ pessoas/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h1>Pessoas</h1>')
+    return render(request, 'index.html')
