96950a2356aa29bebfc34f73cc3326a0c8cd7120
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-24 16:42:00 -0300

    melhorando as urls

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pessoas.urls')),
     path('admin/', admin.site.urls),
-    path('criar', include('pessoas.urls')),
-    path('editar', include('pessoas.urls')),
+    
 ]
