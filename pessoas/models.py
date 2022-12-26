410f341812720d1faa4a46c5535e225d30e39b61
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-25 23:27:03 -0300

    comitando...

--- pessoas/models.py
+++ pessoas/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from datetime import datetime
 
 class Pessoa(models.Model):
-    nome_pessoa = models.CharField(max_length=100)
+    nome_pessoa = models.CharField(max_length=150)
     sobrenome = models.CharField(max_length=100)
     idade = models.PositiveIntegerField()
     data_nascimento = models.DateField()
     email = models.EmailField()
     apelido = models.CharField(max_length=100,blank=True)
     observação = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.nome_pessoa
