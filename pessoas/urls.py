ff59a5ffa5233fcb9dc650a9e19150425e038edd
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-26 03:58:56 -0300

    finalizando...

--- pessoas/urls.py
+++ pessoas/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import IndexView,PessoaCreate
+from .views import IndexView, PessoaCreate, PessoaUpdate
+
 
 urlpatterns = [
    path('', IndexView.as_view(), name=''),
-   path('cadastrar', PessoaCreate.as_view(), name='cadastrar')
+   path('cadastrar', PessoaCreate.as_view(), name='cadastrar'),
+   path('editar/<int:pk>', PessoaUpdate.as_view(), name='editar')
+
+   
 ]
--- pessoas/views.py
+++ pessoas/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView,ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
 from .models import Pessoa
 from django.urls import reverse_lazy
 
@@ -9,6 +9,12 @@ class IndexView(ListView):
 
 
 class PessoaCreate(CreateView):
+    model = Pessoa
+    fields = ['nome_pessoa','sobrenome','idade','data_nascimento','email','apelido','observação']
+    template_name = 'form.html'
+    success_url = reverse_lazy('')
+
+class PessoaUpdate(UpdateView):
     model = Pessoa
     fields = ['nome_pessoa','sobrenome','idade','data_nascimento','email','apelido','observação']
     template_name = 'form.html'
